1f20d427da7f7f7ecf1acf390c2aa485afcacf55
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-01-11 21:50:19 +0530

    Try to reproduce gatsby#10987

---
+++ src/components/image.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.app/gatsby-image
+ * - `StaticQuery`: https://gatsby.app/staticquery
+ */
+
+const Image = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        contentfulAsset(id: { eq: "d3254618-7b35-5d1c-a1cd-cf9f5b1c31fd" }) {
+          id
+          fixed(width: 344, quality: 100) {
+            ...GatsbyContentfulFixed
+          }
+        }
+      }
+    `}
+    render={data => <Img fixed={data.contentfulAsset.fixed} />}
+  />
+)
+export default Image
